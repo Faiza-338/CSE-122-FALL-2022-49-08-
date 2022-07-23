f07cf2d0e91f96b3832fac659d57a8a89d3d9650
Faiza Khandoker <109524293+Faiza-338@users.noreply.github.com>
2022-07-23 23:25:45 +0600

    Add files via upload

---
+++ Lab1/3.338_49-08.cpp
@@ -0,0 +1,21 @@
+//Codeforce Problem No.-231A
+//Problem Name-Team.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, Petya, Vasya, Tonya, number(0);
+    cin >> n;
+    while (n--)
+    {
+        cin >> Petya >> Vasya >> Tonya;
+        if (Petya + Vasya + Tonya >= 2)
+        {
+            number += 1;
+        }
+    }
+    cout << number << endl;
+    return 0;
+}
